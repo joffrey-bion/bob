18aef940ebc90ab06bc40e1bb6c3ea137a8df86b
Joffrey Bion <joffrey.bion@gmail.com>
2021-11-22 01:37:03 +0100

    Upgrade lazysodium dependency (to get it from maven central)

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:1.8.0-beta4")
 
     // necessary for encryption of secrets for GitHub
-    implementation("com.goterl.lazycode:lazysodium-java:4.3.0")
-    implementation("net.java.dev.jna:jna:5.6.0")
+    implementation("com.goterl:lazysodium-java:5.1.1")
+    implementation("net.java.dev.jna:jna:5.10.0")
 
     implementation("com.google.oauth-client:google-oauth-client-jetty:1.31.2")
 }
--- src/main/kotlin/Github.kt
+++ src/main/kotlin/Github.kt
@@ -1,9 +1,9 @@
 package org.hildan.github.secrets.wizard
 
 import com.github.ajalt.clikt.output.TermUi.echo
-import com.goterl.lazycode.lazysodium.LazySodiumJava
-import com.goterl.lazycode.lazysodium.SodiumJava
-import com.goterl.lazycode.lazysodium.utils.Key
+import com.goterl.lazysodium.LazySodiumJava
+import com.goterl.lazysodium.SodiumJava
+import com.goterl.lazysodium.utils.Key
 import io.ktor.client.*
 import io.ktor.client.features.*
 import io.ktor.client.request.*
